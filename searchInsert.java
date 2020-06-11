3e60d4dbfad1594ffadcde637bd6345c112ea5ed
shreya730 <55527424+shreya730@users.noreply.github.com>
2020-06-11 13:31:09 +0530

    Add files via upload

---
+++ searchInsert.java
@@ -0,0 +1,16 @@
+class Solution {         
+    public int searchInsert(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]<target)
+                low=mid+1;
+            else if(nums[mid]>target)
+                high=mid-1;
+            else
+                return mid;
+        }
+        return low;
+    }   
+}
